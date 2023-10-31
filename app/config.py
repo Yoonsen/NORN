3052a6979abec6832a92ffd49951a9d554ffe9a1
Lars Magne Tungland <larsmtungland@gmail.com>
2023-10-31 14:24:12 +0100

    att appdata, Dockerfile

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import os
 
-DATA_BASE_PATH = "../Data"
+DATA_BASE_PATH = "data"
 KONSENSUS_KORPUS_PATH = os.path.join(DATA_BASE_PATH, "konsensus-korpus.xlsx")
 KORPUS_1800_PATH = os.path.join(DATA_BASE_PATH, "1800-1839.xlsx")
 KORPUS_1840_PATH = os.path.join(DATA_BASE_PATH, "1840-1869.xlsx")
